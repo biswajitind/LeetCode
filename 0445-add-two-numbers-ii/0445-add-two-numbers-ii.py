21851d108f60fab0d316accd77f7132d9023e928
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-07-18 15:22:26 -0700

    Time: 7 ms (36.1%), Space: 19.3 MB (43.4%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -4,45 +4,32 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def _print(self, node):
-        while node:
-            print(node.val, " ", end="")
-            node = node.next
-        print("")
-
-    def _reverse(self, node):
-        head  = node
-        nxtNode = node.next
-        head.next = None
-
-        while nxtNode:
-            tmpNode = nxtNode
-            nxtNode = nxtNode.next
-            tmpNode.next = head
-            head = tmpNode
-
-        return(head)
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
-        l1 = self._reverse(l1)
-        l2 = self._reverse(l2)
+        s1, s2 = [], []
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
         carry = 0
-        dummy = head = ListNode()
-        while l1 or l2:
+        head = None
+        while s1 or s2:
             csum = carry
-            if l1:
-                csum += l1.val
-                l1 = l1.next
-            if l2:
-                csum += l2.val
-                l2 = l2.next
+            if s1:
+                csum += s1.pop()
+            if s2:
+                csum += s2.pop()
+            
             carry = csum // 10
             csum = csum % 10
-            head.next = ListNode(val=csum)
-            head = head.next
-            
-        if carry:
-            head.next = ListNode(val=carry)
 
-        return(self._reverse(dummy.next))
+            tmpNode = ListNode(val=csum)
+            tmpNode.next = head
+            head = tmpNode
+        if carry:
+            tmpNode = ListNode(val=carry)
+            tmpNode.next = head
+            head = tmpNode
+        return(head)
