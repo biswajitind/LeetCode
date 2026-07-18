ff8f7fbaaa376b5008e6405e0934189b41673c72
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-07-18 12:53:21 -0700

    Time: 0 ms (100%), Space: 19.2 MB (96.03%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def _print(self, node):
+        while node:
+            print(node.val, " ", end="")
+            node = node.next
+        print("")
+
+    def _reverse(self, node):
+        head  = node
+        nxtNode = node.next
+        head.next = None
+
+        while nxtNode:
+            tmpNode = nxtNode
+            nxtNode = nxtNode.next
+            tmpNode.next = head
+            head = tmpNode
+
+        return(head)
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # self._print(l1)
+        # self._print(self._reverse(l1))
+        # self._print(l2)
+        # self._print(self._reverse(l2))
+        l1 = self._reverse(l1)
+        l2 = self._reverse(l2)
+        carry = 0
+        dummy = head = ListNode()
+        while l1 or l2:
+            csum = carry
+            if l1:
+                csum += l1.val
+                l1 = l1.next
+            if l2:
+                csum += l2.val
+                l2 = l2.next
+            carry = csum // 10
+            csum = csum % 10
+            head.next = ListNode(val=csum)
+            head = head.next
+            
+        if carry:
+            head.next = ListNode(val=carry)
+
+        return(self._reverse(dummy.next))
