cb8f9ed5b6d174fde8f8e599c4c7efdcc756ce33
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-07-21 11:48:52 -0700

    Time: 55 ms (27.26%), Space: 19.3 MB (30.82%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        result = []
+        cache = {0: 1}
+
+        for total in range(1, target + 1):
+            cache[total] = 0
+            for n in nums:
+                cache[total] += cache.get(total - n, 0)
+        
+        return(cache[target])
