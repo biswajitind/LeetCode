c8f81d524f63ff0207e2746e8574b75be484a2a8
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-08-11 21:32:19 -0700

    Time: 0 ms (100%), Space: 20.5 MB (27.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,23 +5,9 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        # The recursive solution is not efficient as It will have addition storage
-        # for each node in stack during each recursive call.
-        # def _rec(cur, prev=None):
-        #     # Base case
-        #     if not cur:
-        #         return(prev)
-        #    
-        #     nxt = cur.next
-        #     cur.next = prev
-        #     return(_rec(nxt, cur))
-        #
-        # return(_rec(head))
-        
-
         prev = None
         curr = head
+
         while curr:
             t = curr.next
             curr.next = prev
