4c2e0d69f43a77ce4066306e778e7778deb0c223
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-08-25 10:27:34 -0700

    Time: 18 ms (5.29%), Space: 19.4 MB (19.84%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,19 +1,31 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {
-            'I':  1,
-            'V':  5,
-            'X':  10,
-            'L':  50,
-            'C':  100,
-            'D':  500,
-            'M':  1000,
-        }
-        res = 0
-        for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
-                res -= roman[s[i]]
-            else:
-                res += roman[s[i]]
-        return(res)
-            
+        mapList = [
+            ['I', '1'],
+            ['IV', '4'],
+            ['V', '5'],
+            ['IX', '9'],
+            ['X', '10'],
+            ['XL', '40'],
+            ['L', '50'],
+            ['XC', '90'],
+            ['C', '100'],
+            ['CD', '400'],
+            ['D', '500'],
+            ['CM', '900'],
+            ['M', '1000']
+        ]
+
+        i = 0 
+        result = 0
+        while i < len(s):
+            for key, val in reversed(mapList):
+                if s[i:].startswith(key):
+                    result += int(val)
+                    i += len(key)
+                    break
+        return(result)
+
+
+
+
