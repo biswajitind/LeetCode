2ecd18160d075f9e4c53e842149a553006fc99d2
Biswajit Dash <35934634+biswajitind@users.noreply.github.com>
2026-09-22 20:28:20 -0700

    Time: 5 ms (15.1%), Space: 21.3 MB (42.79%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -6,6 +6,5 @@ class Solution:
         for i in range(1, len(intervals)):
             if intervals[i][0] < intervals[i-1][1]:
                 return(False)
-        
+                
         return(True)
-        
